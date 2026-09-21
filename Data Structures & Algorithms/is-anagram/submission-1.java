9534b00fd446b7afc2cc3af3305d27de47130f9d
Wei Han Chen <weihanchen0725@gmail.com>
2026-09-21 16:09:55 -0700

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) return false;
+        int length = s.length();
+        char[] sChar = s.toCharArray();
+        char[] tChar = t.toCharArray();
+        Arrays.sort(sChar);
+        Arrays.sort(tChar);
+        for(int index = 0; index < length; index++){
+            if(sChar[index] != tChar[index]) return false;
+        }
+        return true;
+    }
+}
