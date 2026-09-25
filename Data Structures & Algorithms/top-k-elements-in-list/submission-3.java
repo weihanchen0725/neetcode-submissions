4d89908989a994cdaae581dcee1678085c82be03
Wei Han Chen <weihanchen0725@gmail.com>
2026-09-24 22:11:50 -0700

    Add: top-k-elements-in-list - submission-3

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-3.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        HashMap<Integer, Integer> hmap = new HashMap<>();
+        for(int num : nums){
+            hmap.put(num, hmap.getOrDefault(num, 0) + 1);
+        }
+
+        List<Integer> numList = new ArrayList<>(hmap.keySet());
+        numList.sort((a,b) -> Integer.compare(hmap.get(b), hmap.get(a)));
+        
+        int[] result = new int[k];
+        for(int i = 0; i < k; i++){
+            result[i] = numList.get(i);
+        }
+
+        return result;
+    }
+}
